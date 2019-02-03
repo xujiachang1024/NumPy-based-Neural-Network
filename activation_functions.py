dfb993cb6ea4351b02d50ffebf4a05d144d222bc
Ernest Xu 徐嘉昌 <ernest.xu1024@gmail.com>
2019-02-03 10:22:48 +0000

    Debug activation_functions.sigmoid_backward() method

--- activation_functions.py
+++ activation_functions.py
@@ -17,13 +17,27 @@ def sigmoid_forward(Z, debug_mode=False):
 """
 The sigmoid value in backward propagation
 
-@param Z: the NumPy array of the original values, shape = (n_h, m)
+@param A: the NumPy array of the activated values, shape = (n_h, m)
 @param debug_mode: (optional) a boolean value that indicates whether the debug mode is active; the default value is false
 @return a NumPy array of sigmoid values in backward propagation
 """
-def sigmoid_backward(Z, debug_mode=False):
+def sigmoid_backward(A, debug_mode=False):
+    if debug_mode:
+        print("Message: A.shape = " + str(A.shape))
+        print("\tStack trace: activation_functions.sigmoid_backward()")
+    sigmoid_backward = np.multiply(A, (1.0 - A))
+    return sigmoid_backward
+
+def tanh_forward(Z, debug_mode=False):
     if debug_mode:
         print("Message: Z.shape = " + str(Z.shape))
-        print("\tStack trace: activation_functions.sigmoid_backward()")
-    sigmoid_backward = np.multiply(Z, (1.0 - Z))
-    return sigmoid_backward
+        print("\tStack trace: activation_functions.tanh_forward()")
+    tanh_forward = None
+    return tanh_forward
+
+def tanh_backward(Z, debug_mode=False):
+    if debug_mode:
+        print("Message: Z.shape = " + str(Z.shape))
+        print("\tStack trace: activation_functions.tanh_backward()")
+    tanh_backward = None
+    return tanh_backward
